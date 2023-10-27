d49517d7e2a08d4846bb2fc3329ddadcfe8fd16b
Himanshu Pandey <himanshu.dn.pandey@gmail.com>
2023-10-27 19:46:29 +0530

    Add trailing-zeros.cpp

--- trailing-zeros.cpp
+++ trailing-zeros.cpp
@@ -1,3 +1,18 @@
 //
 // Created by himanshu.pandey on 27/10/23.
 //
+#include <iostream>
+using namespace std;
+
+#define ull unsigned long long
+
+int main(){
+    ull n; cin>>n;
+    ull res = 0;
+    while(n > 0){
+        n /= 5;
+        res += n;
+    }
+    cout<<res<<"\n";
+}
+
